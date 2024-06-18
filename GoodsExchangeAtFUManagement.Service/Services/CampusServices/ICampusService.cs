56cb70b26725de480c5686f30b315ac3149e3176
baokhang46 <144251046+baokhang46@users.noreply.github.com>
2024-06-19 00:31:22 +0700

    Campus CRUD

---
+++ GoodsExchangeAtFUManagement.Service/Services/CampusServices/ICampusService.cs
@@ -0,0 +1,21 @@
+﻿using BusinessObjects.DTOs.CampusDTOs;
+using BusinessObjects.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodsExchangeAtFUManagement.Service.Services.CampusServices
+{
+    public interface ICampusService
+    {
+        Task<List<CampusResponseModel>> GetAllCampus();
+        Task<CampusResponseModel> GetCampusById(string id);
+        Task CreateCampus(CampusRequestModel  request);
+        Task UpdateCampus(CampusRequestModel request);
+        Task DeleteCampus(string id);
+
+
+    }
+}
