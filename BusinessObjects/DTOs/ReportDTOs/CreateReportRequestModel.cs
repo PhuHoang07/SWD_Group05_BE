34b1b9dbad7685c7efdf47edcfb9acd9d47e9c52
baokhang46 <144251046+baokhang46@users.noreply.github.com>
2024-07-13 18:51:07 +0700

    [fix] Report v2

--- BusinessObjects/DTOs/ReportDTOs/CreateReportRequestModel.cs
+++ BusinessObjects/DTOs/ReportDTOs/CreateReportRequestModel.cs
@@ -12,6 +12,8 @@ namespace BusinessObjects.DTOs.ReportDTOs
 
         public string ProductPostId { get; set; } = null!;
 
-       
+        public string CreatedBy { get; set; } = null!;
+
+
     }
 }
